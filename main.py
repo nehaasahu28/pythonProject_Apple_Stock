b67f65b1cea889d6de0bc07970b1ce8ddc890d7e
Neha <neha_sahu01@outlook.com>
2021-09-15 21:35:29 +0100

    pc 1

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set(style= "darkgrid")
 import sqlite3 as sql
+
+apple_st = pd.read_csv("APPLE.csv")
+conn = sql.connect("apple.db")
+APPLE = pd.read_sql_query("SELECT * from APPLE", conn)
+conn.close()
+print(apple_st.head(7))
+print("--------------------------------------------------------------")
+print(APPLE.head(7))
+print("--------------------------------------------------------------")
