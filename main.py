5bd214b8f3871e9fe41f8a4866d9864170b32217
Neha <neha_sahu01@outlook.com>
2021-09-18 15:56:16 +0100

    Apple Stock- first visualization

--- main.py
+++ main.py
@@ -2,40 +2,46 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib import style
+#%matplotlib inline
 import seaborn as sns
 sns.set(style= "darkgrid")
 import sqlite3 as sql
-
-apple = pd.read_csv("/Users/neha_gupta/APPLE.csv")
+#importing data
+apple_df = pd.read_csv("/Users/neha_gupta/APPLE.csv",
+                        index_col=0,
+                        parse_dates=True)
 conn = sql.connect("/Users/neha_gupta/apple.db")
-APPLE = pd.read_sql_query("SELECT * from APPLE", conn)
+stock_data = pd.read_sql_query("SELECT * from apple", conn)
 conn.close()
-print(apple.head(420))
-print("-------------------------------------------------------------- \n")
-print(APPLE.head(420))
-print("-------------------------------------------------------------- \n")
+print(apple_df.head())
+print("--------------------------------------------------------------")
+print(stock_data.head())
+print("--------------------------------------------------------------")
+# Describing the Shape of the Data
+print("Apple Stock Details:")
+print(apple_df.shape)
+print(apple_df.dtypes)
+print(apple_df.describe(include="all"))
+print("--------------------------- \n")
 
-print("Apple StockType:")
-print(apple.shape)
-print(apple.dtypes)
-print(apple.describe(include="all"))
-print("-------------------------------------------------------------- \n")
-print("APPLE StockShape: ")
-print(APPLE.shape)
-print(APPLE.dtypes)
-print(APPLE.describe(include="all"))
-print("-------------------------------------------------------------- \n")
+print("Apple shape: ")
+print(stock_data.shape)
+print(stock_data.dtypes)
+print(stock_data.describe(include="all"))
 
-apple.drop(columns='Volume', inplace=True)
-apple.isnull().sum()
+g = sns.pairplot(apple_df[['Open','High','Low']], plot_kws={'color':'#bddc0e'})
 
-apple.dropna(subset = ['Adj Close'], inplace=True)
-print(apple.shape)
-print(apple.isnull().sum())
+#apple.drop(columns='Volume', inplace=True)
+#apple.isnull().sum()
 
-apple["Low"].fillna('Lowest', inplace=True)
+#apple.dropna(subset = ['Adj Close'], inplace=True)
+#print(apple.shape)
+#print(apple.isnull().sum())
 
-print(apple.shape)
-print(apple.isnull().sum())
-print(APPLE.isnull().sum())
+#apple["Low"].fillna('Lowest', inplace=True)
+
+#print(apple.shape)
+#print(apple.isnull().sum())
+#print(APPLE.isnull().sum())
 
